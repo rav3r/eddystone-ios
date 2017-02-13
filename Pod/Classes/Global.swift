be486c53f203b822b7ba9663a288bdb9856b129e
Sam Krantz <sam@bluebite.com>
2017-02-13 13:38:30 -0500

    remove leftovers from stash

--- Pod/Classes/Global.swift
+++ Pod/Classes/Global.swift
@@ -3,15 +3,12 @@ import Foundation
 public var logging = false
 
 func log(_ message: AnyObject) {
-<<<<<<< Updated upstream
     if logging {
         print("[Eddystone] \(message)")
     }
 }
 
 func log(_ message: String) {
-=======
->>>>>>> Stashed changes
     if logging {
         print("[Eddystone] \(message)")
     }
